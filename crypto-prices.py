c067f2b6a8e611dae3a001da32f6ada3deeff63c
Aziz ketata <mohamed.aziz.ketata@gmail.com>
2022-11-18 21:13:53 +0100

    made it send you an email

--- crypto-prices.py
+++ crypto-prices.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 def get_btc_price():
     print("the current prices of digital currency is : ")
@@ -7,10 +10,38 @@ def get_btc_price():
     contenti = url.content
     soup = BeautifulSoup(contenti, 'html.parser')
     # print(soup.prettify())
-    print(type(soup))
+    result ="the current prices of digital currency is : "
     for i, tag in enumerate(soup.find_all('div', attrs={'class': "prices-stripstyles__PricingItemWrapper-sc-19jxxl9-1 fLGzVy"})):
-        print(str(i) + ' :' + "Name of cryptocurrency:" + str(tag.span.text) + ", price:" + str(tag.div.text))
+        result += ("\n"+str(i) + ' :' + "Name of Cryptocurrency:" + str(tag.span.text) + ", Price and Fluctuation: " + str(tag.div.text))
+
+    return result
+
+
+def send_email(body , email_reciever):
+    #enter your email here
+    email_sender = ""
+    #enter password here of sender email
+    email_password = ""
+    #enter the reciever email here
+    email_reciever = ""
+
+    # setting the header of the email
+    subject ="Today's Crypto Prices"
+    em = EmailMessage()
+    em["From"] = email_sender
+    em["To"] = email_reciever
+    em["subject"] = subject
+    em.set_content(body)
+
+    #security using ssl encryption
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com',465, context=context) as smtp:
+        smtp.login(email_sender,email_password)
+        smtp.sendmail(email_sender,email_reciever,em.as_string())
+
 
 if __name__ == '__main__':
-    get_btc_price()
+    body = get_btc_price()
+    email_reciever = ""
+    send_email(body=body ,email_reciever="")
 
